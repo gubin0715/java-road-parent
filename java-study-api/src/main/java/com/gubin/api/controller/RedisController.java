5ccb8ebdb6a1d550d3642f85a348ac6f1faa595a
gubin <gubin0715@163.com>
2019-03-27 18:11:49 +0800

    增加redis

---
+++ java-study-api/src/main/java/com/gubin/api/controller/RedisController.java
@@ -0,0 +1,64 @@
+package com.gubin.api.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.gubin.api.config.redis.RedisService;
+import com.gubin.common.dto.RedisDto;
+import com.gubin.common.dto.ResponseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RestController
+@Api(value = "RedisController", description = "redis相关操作")
+public class RedisController {
+
+    @Autowired
+    private RedisService redisService;
+
+    /**
+     * 存储redis
+     *
+     * @param redisDto
+     * @return
+     */
+    @ApiOperation(value = "存储redis数据", notes = "存储redis数据")
+    @RequestMapping(value = "/setRedis", method = RequestMethod.POST)
+    public ResponseDto setRedis(@RequestBody RedisDto redisDto) {
+        try {
+            redisService.set(redisDto.getKey(), JSON.toJSONString(redisDto));
+            return ResponseDto.SUCCESS();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.ERROR();
+        }
+    }
+
+    /**
+     * 从redis根据key取值
+     *
+     * @param key
+     * @return
+     */
+    @ApiOperation(value = "从redis根据key取值", notes = "从redis根据key取值")
+    @RequestMapping(value = "/getRedis", method = RequestMethod.POST)
+    @ApiImplicitParam(name = "key", value = "存储redis的key值", dataType = "string", required = true, paramType = "query")
+    public ResponseDto getRedis(@RequestParam("key") String key) {
+        try {
+            Object data = redisService.get(key);
+            if (Objects.isNull(data)) {
+                return ResponseDto.ERRORMSG("不存在改key值");
+            } else {
+                RedisDto redisDto = JSONObject.parseObject(data.toString(), RedisDto.class);
+                return ResponseDto.SUCCESSDATA(redisDto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.ERROR();
+        }
+    }
+}
---
+++ java-study-common/src/main/java/com/gubin/common/dto/RedisDto.java
@@ -0,0 +1,11 @@
+package com.gubin.common.dto;
+
+import lombok.Data;
+
+@Data
+public class RedisDto {
+    private String key;
+    private String name;
+    private String age;
+    private String phone;
+}
