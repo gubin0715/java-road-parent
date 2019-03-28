014cf5350ef321b9f9fd06b06129d8698721bb7c
gubin <gubin0715@163.com>
2019-03-28 15:01:59 +0800

    增加nacos

--- java-study-api/src/main/java/com/gubin/api/JavaStudyApiApplication.java
+++ java-study-api/src/main/java/com/gubin/api/JavaStudyApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages = {"com.gubin"})
 @EnableJpaRepositories(basePackages = {"com.gubin.repository"})
 @EntityScan(basePackages = {"com.gubin.common.entity"})
+@EnableDiscoveryClient
 public class JavaStudyApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaStudyApiApplication.class, args);
