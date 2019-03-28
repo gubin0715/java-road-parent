02cce4e369f0825bfdd3db21d2d7ea02c6eb81f4
gubin <gubin0715@163.com>
2019-03-28 16:29:03 +0800

    增加nacos测试动态配置数据

---
+++ java-study-api/src/main/java/com/gubin/api/controller/TestNacosController.java
@@ -0,0 +1,33 @@
+package com.gubin.api.controller;
+
+import com.gubin.common.dto.ResponseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RefreshScope
+@RestController
+@Api(value = "TestNacosController", description = "测试动态刷新配置数据")
+public class TestNacosController {
+
+    @Value("${test_nacos_data}")
+    private String test_nacos_data;
+
+    /**
+     * 测试动态刷新配置数据
+     *
+     * @return
+     */
+    @RequestMapping(value = "testNacosData", method = RequestMethod.POST)
+    @ApiOperation(value = "测试动态刷新配置数据", notes = "测试动态刷新配置数据")
+    public ResponseDto testNacosData() {
+        log.info("测试动态刷新配置数据");
+        return ResponseDto.SUCCESSDATA(test_nacos_data);
+    }
+}
