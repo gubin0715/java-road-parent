3da0ddbdda0b044b3766490bb44a94990ab5ae7c
gubin <gubin0715@163.com>
2019-05-15 15:26:45 +0800

    修改rabbitmq

--- java-study-api/src/main/java/com/gubin/api/controller/TestRabbitMqController.java
+++ java-study-api/src/main/java/com/gubin/api/controller/TestRabbitMqController.java
@@ -1,9 +1,13 @@
 package com.gubin.api.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.gubin.api.config.rabbitmq.MsgProducer;
+import com.gubin.common.dto.RabbitMqDto;
+import com.gubin.common.dto.ResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +21,13 @@ public class TestRabbitMqController {
 
     @ApiOperation(value = "rabbitmq生产消息",notes = "rabbitmq生产消息")
     @RequestMapping(value = "rabbitMq",method = RequestMethod.POST)
-    public String rabbitMq(){
+    public ResponseDto rabbitMq(@RequestBody RabbitMqDto rabbitMqDto){
         try {
-            for(int i=0;i<100;i++) {
-                msgProducer.sendMsg("第A" + i + "条消息");
-            }
+            msgProducer.sendMsg(JSON.toJSONString(rabbitMqDto));
+            return ResponseDto.SUSSCCEMSG("成功");
         } catch (Exception e) {
             e.printStackTrace();
+            return ResponseDto.ERRORMSG("出现异常");
         }
-        return "success";
     }
 }
---
+++ java-study-common/src/main/java/com/gubin/common/dto/RabbitMqDto.java
@@ -0,0 +1,8 @@
+package com.gubin.common.dto;
+
+import lombok.Data;
+
+@Data
+public class RabbitMqDto {
+    private String msg;
+}
