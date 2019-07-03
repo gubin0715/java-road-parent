352e158ef33a9813abd6b94fe88faeb7a50ab5c7
gubin <gubin0715@163.com>
2019-07-03 15:27:20 +0800

    修改拦截器不拦截swagger

--- java-study-api/src/main/java/com/gubin/api/interceptor/WebMvcConfig.java
+++ java-study-api/src/main/java/com/gubin/api/interceptor/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -20,16 +21,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     //增加拦截器
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getInterceptor())    //指定拦截器类
-                .addPathPatterns("/dateabout")
-                .addPathPatterns("/encryptAndDecrypt")
-                .addPathPatterns("/getPostJosnData")
-                .addPathPatterns("/longitudeAndLatitude/**")
-                .addPathPatterns("/mapSoft")
-                .addPathPatterns("/mapListSoft")
-                .addPathPatterns("/mapOut")
-                .addPathPatterns("/aboutString")
-                .addPathPatterns("/userInfoList")
-                .addPathPatterns("/xmlAbout");
+        registry.addInterceptor(getInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
+
+    //如果swagger排除后仍被拦截，放开代码
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
 }
